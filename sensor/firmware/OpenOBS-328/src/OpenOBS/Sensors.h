8a767904309ea48b4ef058480ccc1b52a535ec5f
Ted Langhorst <ted.langhorst@gmail.com>
2025-05-04 14:17:06 -0400

    initial branching

--- sensor/firmware/OpenOBS-328/src/OpenOBS/Sensors.h
+++ sensor/firmware/OpenOBS-328/src/OpenOBS/Sensors.h
@@ -3,15 +3,15 @@
 
 #include <Arduino.h>
 #include <EEPROM.h>
-#include "../libs/Adafruit_VCNL4010/Adafruit_VCNL4010.h"
+#include "../libs/SparkFun_AS7265x/SparkFun_AS7265X.h"
 #include "../libs/MS5803/MS5803.h" 
 
 // Sensor settings structure
 struct SensorSettings {
-    vcnl4010_freq backscatterRate = VCNL4010_250;
-    uint8_t backscatterCurrent = 5;
-    uint8_t ambientAveraging = 1;
-    bool ambientContinuous = true;
+    as7265x_led_current backscatterCurrent = 12; // 12.5, 25, 50, 100 mA
+    as7265x_indicator_current indicatorCurrent = 1; // 1, 2, 4, 8 mA
+    as7265x_gain gain = 1; // 1x, 37x, 16x, 64x
+    uint8_t integrationCycles = 255;
     uint8_t measFlags = 0b00001111;
 };
 
@@ -24,17 +24,16 @@ public:
     uint16_t eepromAddress;  // Store the address passed in constructor
     bool hasPressureSensor;
     SensorSettings settings;    // Settings struct
-    Adafruit_VCNL4010 vcnl4010; // Proximity sensor
+    AS7265X as7265x;            // Proximity sensor
     MS_5803 ms5803;             // Pressure sensor
 
     void saveSettings();
     void loadSettings(); 
-    void setBackscatterRate(vcnl4010_freq rate);
-    void setBackscatterCurrent(uint8_t current);
-    void setAmbientAveraging(uint8_t averaging);
-    void setAmbientContinuous(bool continuous);
+    void setBackscatterCurrent(as7265x_led_current current);
+    void setindicatorCurrent(as7265x_indicator_current current);
+    void setBackscatterGain(as7265x_gain gain);
     void setMeasurementFlags(uint8_t flags);
-    void newSettings(vcnl4010_freq bs_rate, uint8_t bs_current, uint8_t amb_averaging, bool amb_continuous);
+    void newSettings();
     void begin(bool &vcnl_init, bool &ms5803_init); 
     void getReadings(uint16_t &tuAmbient, uint16_t &tuBackscatter, uint32_t &abs_P, int16_t &water_temp);
 };
